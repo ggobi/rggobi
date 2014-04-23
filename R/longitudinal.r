433974c026ee6a69544e6a41b6ef213d87d442bc
hadley <h.wickham@gmail.com>
2014-04-23 12:59:47 -0500

    Supress global var warning

--- R/longitudinal.r
+++ R/longitudinal.r
@@ -2,22 +2,22 @@
 
 # Create longitudinal dataset.
 # Instantiate new ggobi with a longitudinal data set.
-# 
+#
 # This function allows you to load longitudinal data in
 # to GGobi and display it as a line plot.  This is achieved
 # by creating edges between adjacent time points, for a given
 # id variable.
-# 
-# For best viewing, we recommend that you turn the show points off 
-# in the options menu.  When brushing, you may also want to use 
+#
+# For best viewing, we recommend that you turn the show points off
+# in the options menu.  When brushing, you may also want to use
 # categorical brushing on the id variable, so that the entire
 # series is selected for an observation.
-# 
+#
 # @arguments data frame
 # @arguments time variable
 # @arguments id variable
 # @arguments ggobi instance, if you don't want to create a new one
-# @keyword dynamic 
+# @keyword dynamic
 #X data(Oxboys, package="nlme")
 #X ggobi_longitudinal(Oxboys, Occasion, Subject)
 #X ggobi_longitudinal(stormtracks, seasday, id)
@@ -27,7 +27,7 @@ ggobi_longitudinal <- function(data, time=1:rows, id=rep(1, rows), g = NULL) {
 	rows <- nrow(data)
 	time <- eval(substitute(time), data)
 	obsUnit <- eval(substitute(id), data)
-	
+
 	or <- order(obsUnit, time)
 	tmp <- data[or, ]
 	if (is.null(g)) {
@@ -35,17 +35,17 @@ ggobi_longitudinal <- function(data, time=1:rows, id=rep(1, rows), g = NULL) {
 	} else {
 	  g[name] <- tmp
 	}
-	
+
 	edges <- cbind(rownames(tmp[-nrow(tmp), ]), rownames(tmp[-1, ]))
 	matching <- obsUnit[or][-1] == obsUnit[or][-nrow(tmp)]
 	edges[!matching, ] <- NA
-	
+
 	edges <- edges[complete.cases(edges),]
-	
+
 	d <- data.frame(tmp[edges[,1], sapply(tmp, is.factor), drop=FALSE])
 	rownames(d) <- paste(name, 1:nrow(d), sep="")
 	g[paste(name, "edges", sep="-")] <- d
-	edges(g[paste(name, "edges", sep="-")]) <- edges 
+	edges(g[paste(name, "edges", sep="-")]) <- edges
 
 	if (!is.null(ggobi)) {
     d <- displays(g)[[1]]
@@ -57,21 +57,22 @@ ggobi_longitudinal <- function(data, time=1:rows, id=rep(1, rows), g = NULL) {
 
 # Create parallel coordinates plot.
 # Mock up a pcp plot using points and edges.
-# 
+#
 # Experimental and may suggest ways to reduce PCP code in GGobi
-# 
+#
 # @keyword internal
 ggobi_pcp <- function(data, type="range") {
   if (!require(reshape)) stop("Must have reshape package installed")
   data$CASEID <- factor(1:nrow(data))
   datam <- melt(rescaler(data, type=type), id = "CASEID")
-  
+
   g <- ggobi_longitudinal(datam, id=CASEID)
   g$raw <- data
 
   d <- displays(g)[[1]]
   edges(d) <- g[1]
   variables(d) <- list(X="variable", Y="value")
-  
+
   invisible(g)
 }
+globalVariables("CASEID")
